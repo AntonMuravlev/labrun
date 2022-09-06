ecf11c02e8b33b47faffa4a523c825017fafe35d
AntonMuravlev <antonforward@gmail.com>
2022-09-06 14:00:53 +0300

    minor logger fix

--- labrun/core/util.py
+++ labrun/core/util.py
@@ -1,9 +1,10 @@
 import logging
 from pkg_resources import resource_filename
+from datetime import datetime
 
 def log_config(module_name):
-    log = logging.getLogger(module_name)
-    log.setLevel(logging.DEBUG)
+    logger = logging.getLogger(module_name)
+    logger.setLevel(logging.DEBUG)
 
     # create formatters
     formatter_stderr = logging.Formatter(
@@ -12,6 +13,7 @@ def log_config(module_name):
     formatter_file = logging.Formatter(
         "{asctime} - {name} - {levelname} - {message}", style="{"
     )
+    current_time = datetime.now().strftime('%Y_%m_%d-%H:%M:%S')
 
     # create stderr handler
     stderr = logging.StreamHandler()
@@ -19,18 +21,18 @@ def log_config(module_name):
     stderr.setFormatter(formatter_stderr)
 
     # create file handler (common log for all modules)
-    log_filename = resource_filename("labrun", "logs/common_labrun.log")
+    log_filename = resource_filename("labrun", f"logs/{current_time}_common_labrun.log")
     logfile_common = logging.FileHandler(log_filename)
     logfile_common.setLevel(logging.DEBUG)
     logfile_common.setFormatter(formatter_file)
 
     # create file handler (dedicated log for module)
-    log_filename = resource_filename("labrun", f"logs/{module_name}.log")
+    log_filename = resource_filename("labrun", f"logs/{current_time}_{module_name}.log")
     logfile_module = logging.FileHandler(log_filename)
     logfile_module.setLevel(logging.DEBUG)
     logfile_module.setFormatter(formatter_file)
 
-    log.addHandler(stderr)
-    log.addHandler(logfile_common)
-    log.addHandler(logfile_module)
-    return log
+    logger.addHandler(stderr)
+    logger.addHandler(logfile_common)
+    logger.addHandler(logfile_module)
+    return logger
