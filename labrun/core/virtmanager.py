95c24dfb2f06376de9764b2be1b0a72db26f7c2b
AntonMuravlev <antonforward@gmail.com>
2022-12-21 15:28:09 +0300

    add virtmanager

---
+++ labrun/core/virtmanager.py
@@ -0,0 +1,10 @@
+class VirtManagerBase:
+    pass
+
+
+class VirtManagerClab(VirtManagerBase):
+    def craft_topo_file(self, lab_name, nodes, topology):
+        pass
+
+    def deploy_lab(self):
+        pass
